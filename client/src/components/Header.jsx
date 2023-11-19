9e01c2872deb918c6f3a8a4169863dab695ce46f
Devraj <khanradevraj@gmail.com>
2023-11-19 11:18:06 +0530

    server created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,15 +23,21 @@ export default function Header() {
 
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </Link>
 
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </Link>
 
           <Link to="/signin">
-            <li className="hidden sm:inline text-slate-700 hover:underline">SignIn</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              SignIn
+            </li>
           </Link>
         </ul>
       </div>
