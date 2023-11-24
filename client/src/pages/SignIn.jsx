c614743ff59d7cc694c223df30cad9af71cb7c73
Devraj <khanradevraj@gmail.com>
2023-11-24 12:31:12 +0530

    added redux toolkit, created store, userSlice.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../store/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector( (state) => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -18,7 +24,7 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      setLoading(true);
+      dispatch(signInStart());
 
       const response = await fetch("/api/auth/signin", {
         method: "POST",
@@ -30,24 +36,20 @@ export default function SignIn() {
 
       const data = await response.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message))
     }
   };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">SignIn</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        
         <input
           type="email"
           placeholder="email"
@@ -69,12 +71,12 @@ export default function SignIn() {
           type="submit"
           className="bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading..." : "Sign Up"}
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
 
       <div className="flex gap-2 mt-5">
-        <p>Do not have an account?</p>
+        <p>No account?</p>
         <Link to={"/signup"}>
           <span className="text-blue-700">Sign Up</span>
         </Link>
