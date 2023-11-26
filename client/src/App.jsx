2282fb386528641c479d67db71abaefbe9bfbb2e
Devraj <khanradevraj@gmail.com>
2023-11-26 20:22:32 +0530

    Made Private Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
 // import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import Profile from './pages/Profile'
-import About from './pages/About'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  
-
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/signin' element={<SignIn />}/>
-      <Route path='/signup' element={<SignUp />}/>
-      <Route path='/profile' element={<Profile />}/>
-      <Route path='/about' element={<About />}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+        
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,46 +1,53 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <header className="bg-slate-100 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to="/">
-          <h1 className="font-semibold text-sm sm:text-4xl flex flex-wrap">
-            <span className="text-blue-900">Real</span>
-            <span className="text-blue-400">Estate</span>
-          </h1>
-        </Link>
+    <>
+      {currentUser ? (
+        <header className="bg-slate-100 shadow-md">
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <Link to="/">
+              <h1 className="font-semibold text-sm sm:text-4xl flex flex-wrap">
+                <span className="text-blue-900">Real</span>
+                <span className="text-blue-400">Estate</span>
+              </h1>
+            </Link>
 
-        <form className="bg-slate-50 p-3 rounded-3xl flex items-center">
-          <input
-            type="search"
-            placeholder="Search"
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
-          />
-          <FaSearch className="text-slate-400" />
-        </form>
+            <form className="bg-slate-50 p-3 rounded-3xl flex items-center">
+              <input
+                type="search"
+                placeholder="Search"
+                className="bg-transparent focus:outline-none w-24 sm:w-64"
+              />
+              <FaSearch className="text-slate-400" />
+            </form>
 
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              Home
-            </li>
-          </Link>
+            <ul className="flex gap-4">
+              <Link to="/">
+                <li className="hidden sm:inline text-slate-700 hover:underline">
+                  Home
+                </li>
+              </Link>
 
-          <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              About
-            </li>
-          </Link>
+              <Link to="/about">
+                <li className="hidden sm:inline text-slate-700 hover:underline">
+                  About
+                </li>
+              </Link>
 
-          <Link to="/signin">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              SignIn
-            </li>
-          </Link>
-        </ul>
-      </div>
-    </header>
+              <Link to="/profile">
+                <img className="rounded-full h-6 w-6" src={currentUser.avatar} alt="profile"/>
+              </Link>
+            </ul>
+          </div>
+        </header>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
